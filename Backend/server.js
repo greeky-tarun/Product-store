c8b3f892d3e53318f0d8f246508e761f6fbf6e4b
B. Tarun Kumar <btarunkumar7010@gmail.com>
2024-11-12 23:28:42 +0530

    added json data

--- Backend/server.js
+++ Backend/server.js
@@ -10,8 +10,11 @@ dotenv.config();
 //function calling
 const app = express();
 
+//allow us to accept json data in the req.body
+app.use(express.json());
+
 //route method post is basically when we create something it will store
-app.post("/products", async(req, res) =>{
+app.post("/api/products", async(req, res) =>{
     const product = req.body; //user will send this data
     if(!product.name || !product.price || !product.image){
         return res.status(400).json({success: false, message: "All fields are required"});
@@ -23,15 +26,13 @@ app.post("/products", async(req, res) =>{
         await newProduct.save();
         res.status(201).json({success: true, data: newProduct});
     } catch (error) {
-        console.error(error);
+        console.error("Error in creating product:", error.message);
         res.status(500).json({success: false, message: "Internal server error"});
     }
 });
 
 // Call the mongo URI from .env
 // console.log(process.env.MONGO_URI);
-
-
 
 //port
 app.listen(5000, ()=> {
